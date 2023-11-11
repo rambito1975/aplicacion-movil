541a873170c2dc4f4005b08857e21e036b6ac553
AzirDeNazzaret <die.jimenezc@duocuc.cl>
2023-11-11 18:39:52 -0300

    Firebase
    usabilidad login y registro con firebase

---
+++ src/app/services/utils.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, inject } from '@angular/core';
+import { LoadingController, ToastController, ToastOptions } from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UtilsService {
+
+  loadingCtrl = inject(LoadingController);
+  toastCtrl = inject(ToastController);
+
+  //=========== Loading =============
+  loading() {
+    return this.loadingCtrl.create({ spinner: 'crescent' })
+  }
+
+  //========= toast ===========
+  async presentToast(opts?: ToastOptions) {
+    const toast = await this.toastCtrl.create(opts);
+    toast.present();
+  }
+
+
+
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebaseConfig:{
+    apiKey: "AIzaSyAjIet8uU-V8pyJXNHp7pE2ehrMRPBIARk",
+    authDomain: "duocpresent.firebaseapp.com",
+    projectId: "duocpresent",
+    storageBucket: "duocpresent.appspot.com",
+    messagingSenderId: "413651213306",
+    appId: "1:413651213306:web:d038477ed63362da23e243"
+  }
 };
