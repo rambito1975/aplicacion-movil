59473351f1081847db04d25213da54d5a46576bb
rambito <ma.hermosillav@duocuc.cl>
2023-11-12 19:06:55 -0300

    Ingresa usurios verificados

--- src/app/pages/auth/sign-up/sign-up.page.ts
+++ src/app/pages/auth/sign-up/sign-up.page.ts
@@ -11,6 +11,7 @@ import { UtilsService } from 'src/app/services/utils.service';
 export class SignUpPage implements OnInit {
 
   form = new FormGroup({
+    uid: new FormControl(''),
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required]),
     name: new FormControl('', [Validators.required, Validators.minLength(4)]),
@@ -30,8 +31,46 @@ export class SignUpPage implements OnInit {
       await loading.present();
 
       this.firebaseSvc.signUp(this.form.value as User).then(async res =>{
-        await this.firebaseSvc.updateUser(this.form.value.name)
-        console.log(res);
+        await this.firebaseSvc.updateUser(this.form.value.name);
+
+        let uid = res.user.uid;
+        this.form.controls.uid.setValue(uid);
+
+        this.setUserInfo(uid);
+
+        
+      }).catch(error => {
+        console.log(error);
+
+        this.utilsSvc.presentToast({
+          message: error.message,
+          duration: 2500,
+          color: 'primary',
+          position: 'middle',
+          icon:'alert-circle-outline'
+        })
+        
+      }).finally(() => {
+        loading.dismiss();
+      })
+    }
+
+  }
+
+  async setUserInfo(uid: string) {
+    if (this.form.valid) {
+
+      const loading = await this.utilsSvc.loading();
+      await loading.present();
+
+      let path = 'user/${uid}';
+      delete this.form.value.password;
+
+      this.firebaseSvc.setDocument(path, this.form.value).then(async res =>{
+      
+        this.utilsSvc.saveInLocalStorage('user', this.form.value)
+        this.utilsSvc.routerLink('/pagina1');
+        this.form.reset();
         
       }).catch(error => {
         console.log(error);
